1f74bafaec953e1febc70778e5aa84b2a3916cdc
emilynelson-du <114930183+emilynelson-du@users.noreply.github.com>
2023-06-08 18:06:49 +0100

    https

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://quiz-api-en.us-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://quiz-api-en.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
